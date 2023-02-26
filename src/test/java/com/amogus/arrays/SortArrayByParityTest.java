27f647ef405d3a152f30cdebad25d7bb723c0e0c
holydrug <nonprofitxzbusiness@gmail.com>
2023-02-26 14:09:53 +0300

    added 905. Sort Array By Parity

---
+++ src/test/java/com/amogus/arrays/SortArrayByParityTest.java
@@ -0,0 +1,26 @@
+package com.amogus.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class SortArrayByParityTest {
+    private SortArrayByParity underTest;
+
+    @BeforeEach
+    public void init() {
+        underTest = new SortArrayByParity();
+    }
+
+    @Test
+    void sortArrayByParity() {
+        int[] input = new int[]{1, 0};
+        int[] output = new int[]{0, 1};
+
+        int[] result = underTest.sortArrayByParity(input);
+
+        Assertions.assertTrue(Arrays.equals(output, result));
+    }
+}
