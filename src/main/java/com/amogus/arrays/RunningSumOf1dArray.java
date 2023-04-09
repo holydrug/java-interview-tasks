4319b0d1e9fff13bc9724fd26ac8a8258d46bf19
holydrug <nonprofitxzbusiness@gmail.com>
2023-04-09 19:00:40 +0300

    added 1480  Running Sum of 1d Array

---
+++ src/main/java/com/amogus/arrays/RunningSumOf1dArray.java
@@ -0,0 +1,37 @@
+package com.amogus.arrays;
+
+/**
+ * 1480. Running Sum of 1d Array
+ * https://leetcode.com/problems/running-sum-of-1d-array/description/
+ * ==
+ * Given an array nums.
+ * We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ * Return the running sum of nums.
+ * ==
+ * Example 1:
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ * ==
+ * Example 2:
+ * Input: nums = [1,1,1,1,1]
+ * Output: [1,2,3,4,5]
+ * Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+ * ==
+ * Example 3:
+ * Input: nums = [3,1,2,10,1]
+ * Output: [3,4,6,16,17]
+ */
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        int[] runningSumArray = new int[nums.length];
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            runningSumArray[i] = sum;
+        }
+
+        return runningSumArray;
+    }
+}
