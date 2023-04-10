366c047b502c08866662bb98e0ac2714e0c19e4c
holydrug <nonprofitxzbusiness@gmail.com>
2023-04-10 21:49:03 +0300

    added 1757. Recyclable and Low Fat Products

---
+++ src/main/java/com/amogus/sql/RecyclableAndLowFatProducts.sql
@@ -0,0 +1,4 @@
+1757. Recyclable and Low Fat Products
+https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+select Products.product_id from Products where low_fats = 'Y' and recyclable = 'Y'
