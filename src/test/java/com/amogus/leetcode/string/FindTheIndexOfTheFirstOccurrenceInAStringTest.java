31fd150640dd027a5ec6767d1d60a0f1489a15d0
holydrug <nonprofitxzbusiness@gmail.com>
2023-05-09 23:14:05 +0300

    added 28. Find the Index of the First Occurrence in a String

---
+++ src/test/java/com/amogus/leetcode/string/FindTheIndexOfTheFirstOccurrenceInAStringTest.java
@@ -0,0 +1,49 @@
+package com.amogus.leetcode.string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTheIndexOfTheFirstOccurrenceInAStringTest {
+
+    private FindTheIndexOfTheFirstOccurrenceInAString underTest;
+
+    @BeforeEach
+    public void init() {
+        underTest = new FindTheIndexOfTheFirstOccurrenceInAString();
+    }
+
+    @Test
+    void strStr1() {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        int output = 0;
+
+        int result = underTest.strStr(haystack, needle);
+
+        Assertions.assertEquals(output, result);
+    }
+
+    @Test
+    void strStr2() {
+        String haystack = "abcabeabcabcabd";
+        String needle = "abcabd";
+        int output = 9;
+
+        int result = underTest.strStr(haystack, needle);
+
+        Assertions.assertEquals(output, result);
+    }
+
+    @Test
+    void prefixFunc() {
+        String input = "abcabd";
+        int[] output = new int[] {0, 0, 1, 2};
+
+        int[] result = underTest.prefixFunc(input);
+
+        Assertions.assertArrayEquals(output, result);
+    }
+}
