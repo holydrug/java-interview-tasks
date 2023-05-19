96c4dbc0456de0fcc2129d15b2c24708d051788f
holydrug <nonprofitxzbusiness@gmail.com>
2023-05-19 17:58:19 +0300

    added 209. Minimum Size Subarray Sum

---
+++ src/test/java/com/amogus/leetcode/arrays/sliding/window/MinimumSizeSubarraySumTest.java
@@ -0,0 +1,49 @@
+package com.amogus.leetcode.arrays.sliding.window;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MinimumSizeSubarraySumTest {
+
+    private MinimumSizeSubarraySum underTest;
+
+    @BeforeEach
+    public void init() {
+        underTest = new MinimumSizeSubarraySum();
+    }
+
+    @Test
+    void minSubArrayLen1() {
+        int[] input = new int[]{2, 3, 1, 2, 4, 3};
+        int target = 7;
+        int output = 2;
+
+        int result = underTest.minSubArrayLen(target, input);
+
+        Assertions.assertEquals(output, result);
+    }
+
+    @Test
+    void minSubArrayLen2() {
+        int[] input = new int[]{1, 4, 4};
+        int target = 4;
+        int output = 1;
+
+        int result = underTest.minSubArrayLen(target, input);
+
+        Assertions.assertEquals(output, result);
+    }
+
+    @Test
+    void minSubArrayLen3() {
+        int[] input = new int[]{11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int target = 11;
+        int output = 1;
+
+        int result = underTest.minSubArrayLen(target, input);
+
+        Assertions.assertEquals(output, result);
+    }
+
+}
